d7a04008bfb8aa5242e020107ecf57288cf01bc0
Minseong <Minseong0531>
2025-07-02 13:54:04 +0900

    fix url

--- src/component/MainBanner.js
+++ src/component/MainBanner.js
@@ -6,15 +6,15 @@ function MainBanner(){
     return(
         <div className="main_banner">
                 <div className="banner_wrap">
-                    <img src={fixPath('images/Contents/main_banner.png')} />
+                    <img src={fixPath('images/contents/main_banner.png')} alt="메인 배너" />
                 
                 <div className="banner_icons">
                     <ul>
-                        <li><Link to="/" /><img src={fixPath("images/icon/Steam.png")}/></li>
-                        <li><Link to="/" /><img src={fixPath("images/icon/X.png")}/></li>
-                        <li><Link to="/" /><img src={fixPath("images/icon/Youtube.png")}/></li>
+                        <li><Link to="/"><img src={fixPath("images/icon/Steam.png")}/></Link></li>
+                        <li><Link to="/"><img src={fixPath("images/icon/X.png")}/></Link></li>
+                        <li><Link to="/"><img src={fixPath("images/icon/Youtube.png")}/></Link></li>
                     </ul>
-                    <img src={fixPath("images/icon/Scrolldown.png")} />
+                    <img src={fixPath("images/icon/Scrolldown.png")} alt="스크롤 다운" />
                 </div>
             </div>
         </div>
